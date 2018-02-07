aa7cd394fe52f6ba6b7157ad408f5024f9104c16
Shubham <shubham0522@gmail.com>
2018-02-08 02:24:31 +0530

    image

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://damp-mesa-30877.herokuapp.com/';
+$config['base_url'] = 'https://dracoweb.herokuapp.com/';
 
 
 /*
