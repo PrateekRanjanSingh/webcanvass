4637e4df6f38cda04e2d39524f12ec0cd1f7a6b8
Shubham <shubham0522@gmail.com>
2018-02-07 19:13:03 +0530

    Databseconfig

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
-	'database' => 'OSP',
-	'dbdriver' => 'mysqli',
+	'hostname' => 'ec2-50-19-105-188.compute-1.amazonaws.com:5432',
+	'username' => 'axzbdamrofwcwr',
+	'password' => '0346e42df5885462bfb4ee830bd1f955138b887d55b11a43695ef62d9158a52d',
+	'database' => 'd6ukjaplr8h2j2',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
@@ -94,3 +94,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
